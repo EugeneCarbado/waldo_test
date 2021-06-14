5a7a12e1fb079d168e431adefe167248d996966b
eugene <eugenecarbado@gmail.com>
2021-06-14 12:20:36 +0100

    .

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,10 +10,10 @@ import "swiper/components/pagination/pagination.min.css";
 
 // Images for slides
 import imageLimited from '../images/limited_time_mobile.svg';
-import image1 from '../images/sunglasses_box_mobile.svg';
-import image2 from '../images/waldo_blue_box_mobile.svg';
-import image3 from '../images/lady_shades_mobile.svg';
-import image4 from '../images/guy_shades_mobile.svg';
+import image1 from '../images/sunglasses_box_mobile.png';
+import image2 from '../images/waldo_blue_box_mobile.png';
+import image3 from '../images/female_model_sunglasses_mobile.png';
+import image4 from '../images/male_model_sunglasses_mobile.png';
 
 // install Swiper components
 SwiperCore.use([ Pagination ]);
@@ -30,10 +30,10 @@ const Slider = () => {
     >
       <SwiperSlide className="static">
           <img className="absolute top-72 left-16" src={imageLimited} alt="limited time only" />
-          <img className="absolute top-40 left-5" src={image1} alt="waldo blue light glasses" />
+          <img className="absolute top-40 right-1 transform scale-75" src={image1} alt="waldo blue light glasses" />
       </SwiperSlide>
       <SwiperSlide className="static">
-          <img className="absolute top-40 left-10" src={image2} alt="waldo package" />
+          <img className="absolute top-40 transform scale-75" src={image2} alt="waldo package" />
       </SwiperSlide>
       <SwiperSlide>
           <img src={image3} alt="waldo lady shades" />
